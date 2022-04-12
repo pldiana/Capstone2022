f632418b3290b016ae0cde079c688d2990a5136f
Kevin3142 <kevin3142@yahoo.com>
2022-04-11 19:31:50 -0500

    Added NewsStories.cs

---
+++ Models/NewsStories.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Models
+{
+    public class Article
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public string url { get; set; }
+        public string urlToImage { get; set; }
+        public DateTime publishedAt { get; set; }
+        public string content { get; set; }
+    }
+
+}
+
