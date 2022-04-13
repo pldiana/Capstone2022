45eef38a991a0a045088173723da569bec6425ec
Kevin3142 <kevin3142@yahoo.com>
2022-04-12 19:30:44 -0500

    updating dashboard and add newsstories model

--- Models/NewsStories.cs
+++ Models/NewsStories.cs
@@ -1,15 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace Models
 {
-    public class Article
-    {
-        public string title { get; set; }
-        public string description { get; set; }
-        public string url { get; set; }
-        public string urlToImage { get; set; }
-        public DateTime publishedAt { get; set; }
-        public string content { get; set; }
-    }
-
+    public class Source
+{
+    public object id { get; set; }
+    public string name { get; set; }
 }
 
+public class Article
+{
+    public Source source { get; set; }
+    public string author { get; set; }
+    public string title { get; set; }
+    public string description { get; set; }
+    public string url { get; set; }
+    public string urlToImage { get; set; }
+    public DateTime publishedAt { get; set; }
+    public string content { get; set; }
+}
+
+public class Root
+{
+    public string status { get; set; }
+    public int totalResults { get; set; }
+    public List<Article> Articles { get; set; }
+}
+}
